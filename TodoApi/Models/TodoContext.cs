a40139dafb49194fb4dab696a45ad0334f6c3dad
GORETEE VIRIDIANA AVILA CIENEGA <goretee.avilac@gto.cecyteg.edu.mx>
2024-03-20 14:25:38 -0600

    Agregar archivos de proyecto.

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        //contructor-contexto que utilizamos para funcinar el scanffold
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
